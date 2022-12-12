d1d53f9b7f04a15f21a7187a37d47e8e82a50c51
Justin  Baskaran <justinbaskaran5@gmail.com>
2022-12-12 14:13:20 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { fetchDailyDataCalories,fetchDailyDataSpeed } from "./Data";
+import { fetchDailyDataCalories,fetchDailyDataSpeed,fetchDailyDataHeartRate } from "./Data";
 import Chart from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 
 const LineChart = () => {
   const [dailyDataCalories, setDailyDataCalories] = useState([]);
   const [dailyDataSpeed, setDailyDataSpeed] = useState([]);
-  
+  const [dailyDataHeartRate, setDailyDataHeartRate] = useState([]);
+
   const fetchApi = async () => {
     const dailyDataCalories = await fetchDailyDataCalories();
     setDailyDataCalories(dailyDataCalories);
     const dailyDataSpeed = await fetchDailyDataSpeed();
     setDailyDataSpeed(dailyDataSpeed);
+    const dailyDataHeartRate = await fetchDailyDataHeartRate();
+    setDailyDataHeartRate(dailyDataHeartRate);
   };
 
 
@@ -27,7 +30,7 @@ const LineChart = () => {
         datasets: [
           {
             data: Array.from(dailyDataCalories.entries()),
-            label: "Calories",
+            label: "Calories (calories)",
             borderColor: 'rgb(75, 192, 192)',
             fill: false,
           }
@@ -55,12 +58,30 @@ const LineChart = () => {
     
   ) : null;
 
+  const lineChartHeartRate = dailyDataHeartRate  ? (
+
+    <Line
+      data={{
+        labels: Array.from(dailyDataHeartRate.keys()),
+        datasets: [
+          {
+            data: Array.from(dailyDataHeartRate.entries()),
+            label: "Mean Heart Rate (beats per minute)",
+            borderColor: 'rgb(75, 192, 192)',
+            fill: false,
+          }
+        ],
+      }}
+    />
+    
+  ) : null;
 
   return (
     <>
       <center><h1>Swimming Metrics</h1></center>
       <div>{lineChartCalories}</div>
       <div>{lineChartSpeed}</div>
+      <div>{lineChartHeartRate}</div>
     </>
   );
 };
